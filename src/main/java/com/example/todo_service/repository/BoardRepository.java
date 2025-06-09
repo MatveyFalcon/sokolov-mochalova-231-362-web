30c9df19ca4b4a270c0223a0f7c14dc78336a39f
Matvey <matveyfalcon@gmail.com>
2025-06-09 16:31:28 +0300

    add repositories for Board and Task entities

---
+++ src/main/java/com/example/todo_service/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.example.todo_service.repository;
+
+import com.example.todo_service.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
